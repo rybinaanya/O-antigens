ecca427135a433ddd56565fdae8596e0dcb822bf
Anna <70780981+AnyaChurkina@users.noreply.github.com>
2021-05-29 18:49:29 +0300

    Update draw_conserved_operon_prokka.py

--- operon_visualization/draw_conserved_operon_prokka.py
+++ operon_visualization/draw_conserved_operon_prokka.py
@@ -16,7 +16,14 @@ def print_operon(prokka_gb, start, stop):
 
 
 def visualize_conserved_operon(prokka_gff, prokka_gb, operon):
-
+    """
+    Function produces schematic plot of DNA region with boundaries determined
+    by searching for the coordinates of the outermost genes of the operon;
+    :param prokka_gff: path to GFF annotation file from Prokka
+    :param prokka_gb: path to GB annotation file from Prokka
+    :param operon: name of conserved operon: gal or wec
+    :return: None
+    """
     # find_conserved_operon_boundaries
     conserved_operon_boundaries = []
     with open(prokka_gff) as gff:
@@ -26,25 +33,35 @@ def visualize_conserved_operon(prokka_gff, prokka_gb, operon):
             if "gene" in line:
                 line = line.strip().split()
                 info = line[8]
+                start = line[3]
+                stop = line[4]
                 strand = line[6]
+
+                # find_coordinate_of_first_gene_in_operon
                 if operon[0] in info:
                     if strand == "+":
-                        conserved_operon_start = int(line[3])
+                        # if gene in + strand, first coordinate of gene is conserved operon start
+                        conserved_operon_start = int(start)
                         conserved_operon_boundaries.append(
                             conserved_operon_start)
                     else:
-                        conserved_operon_start = int(line[4])
+                        # else second coordinate of gene is conserved operon start
+                        conserved_operon_start = int(stop)
                         conserved_operon_boundaries.append(
                             conserved_operon_start)
+
+                # find_coordinate_of_first_gene_in_operon
                 if operon[-1] in info:
                     if strand == "+":
-                        conserved_operon_start = int(line[4])
+                        # if gene in + strand, second coordinate of gene is conserved operon end
+                        conserved_operon_stop = int(stop)
                         conserved_operon_boundaries.append(
-                            conserved_operon_start)
+                            conserved_operon_stop)
                     else:
-                        conserved_operon_start = int(line[3])
+                        # else second coordinate of gene is conserved operon end
+                        conserved_operon_stop = int(start)
                         conserved_operon_boundaries.append(
-                            conserved_operon_start)
+                            conserved_operon_stop)
 
         print_operon(prokka_gb, conserved_operon_boundaries[0]-1,
                      conserved_operon_boundaries[1])
