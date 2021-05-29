a8ba5424448370bb7b2abf246234f0b2e9e73f86
Anna <70780981+AnyaChurkina@users.noreply.github.com>
2021-05-29 21:11:12 +0300

    Update extract_sequence.py

--- extract_sequence.py
+++ extract_sequence.py
@@ -1,11 +1,18 @@
 from Bio import SeqIO
 
 
-def extract_sequence(path_to_fasta, start, stop, strand):
+def extract_gene(path_to_fasta, start, stop, strand):
+    """
+    Function extract gene sequence from fasta file.
+    Useful for BLAST searching.
+    :param path_to_fasta: path to genome FASTA file
+    :param start: start coordinate of desired fragment
+    :param stop: end coordinate of desired fragment
+    :param strand: '+' or '-' DNA strand on which fragment is located
+    :return: sequence of desired DNA fragment
+    """
     for fasta in SeqIO.parse(path_to_fasta, "fasta"):
-
-        # 300 nucleotides added to every border to search for promoter and terminator
-        sequence = fasta[start - 300: stop + 300]
+        sequence = fasta[start: stop]
         if strand == '+':
             return sequence.seq
         else:
