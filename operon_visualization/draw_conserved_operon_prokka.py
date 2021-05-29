008ceb3ee59b4bc8a789bd735d078e0442b63af6
Anna <70780981+AnyaChurkina@users.noreply.github.com>
2021-05-29 18:29:24 +0300

    Add files via upload

---
+++ operon_visualization/draw_conserved_operon_prokka.py
@@ -0,0 +1,56 @@
+from matplotlib import pyplot as plt
+from dna_features_viewer import BiopythonTranslator
+
+
+wec_operon = ['wecA', 'wecB', 'rffG', 'rffH', 'wecD', 'wecE', 'wzxE', 'wecF',
+              'wzyE', 'wzyE', 'wecG']
+
+gal_operon = ['galE', 'galT', 'galK', 'galM', 'galR']
+
+
+def print_operon(prokka_gb, start, stop):
+    graphic_record = BiopythonTranslator().translate_record(prokka_gb)
+    operon = graphic_record.crop((start, stop))
+    operon.plot(figure_width=10, strand_in_label_threshold=7)
+    plt.show()
+
+
+def visualize_conserved_operon(prokka_gff, prokka_gb, operon):
+
+    # find_conserved_operon_boundaries
+    conserved_operon_boundaries = []
+    with open(prokka_gff) as gff:
+        for line in gff:
+            if line.startswith("#"):
+                continue
+            if "gene" in line:
+                line = line.strip().split()
+                info = line[8]
+                strand = line[6]
+                if operon[0] in info:
+                    if strand == "+":
+                        conserved_operon_start = int(line[3])
+                        conserved_operon_boundaries.append(
+                            conserved_operon_start)
+                    else:
+                        conserved_operon_start = int(line[4])
+                        conserved_operon_boundaries.append(
+                            conserved_operon_start)
+                if operon[-1] in info:
+                    if strand == "+":
+                        conserved_operon_start = int(line[4])
+                        conserved_operon_boundaries.append(
+                            conserved_operon_start)
+                    else:
+                        conserved_operon_start = int(line[3])
+                        conserved_operon_boundaries.append(
+                            conserved_operon_start)
+
+        print_operon(prokka_gb, conserved_operon_boundaries[0]-1,
+                     conserved_operon_boundaries[1])
+
+
+# Example
+visualize_conserved_operon("GCA_016618195.1.gff",
+                           "GCA_016618195.1.gbf",
+                           wec_operon)
---
+++ operon_visualization/draw_operon_from_gb.py
@@ -0,0 +1,77 @@
+from matplotlib import pyplot as plt
+from dna_features_viewer import BiopythonTranslator
+from Bio import SeqIO
+
+
+def draw_operon_one_contig_assembly(gb_file, annotation, start, stop):
+    """
+    Function produces schematic plot of DNA region specified with coordinates
+    by any (Prokka or PGAP) annotation of assembly in one contig;
+    :param gb_file: path to GB annotation file
+    :param annotation: name annotation: Prokka or PGAP
+    :param start: start coordinate of the desired region
+    :param stop: end coordinate of the desired region
+    :return: None
+    """
+
+    if annotation == "PGAP":
+        graphic_translator = BiopythonTranslator()
+        graphic_translator.ignored_features_types = ['CDS']
+        graphic_record = graphic_translator.translate_record(gb_file)
+        graphic_record.crop((start, stop)).plot(figure_width=10,
+                                                strand_in_label_threshold=7)
+        plt.show()
+
+    if annotation == "Prokka":
+        graphic_record = BiopythonTranslator().translate_record(gb_file)
+        operon = graphic_record.crop((start, stop))
+        operon.plot(figure_width=10, strand_in_label_threshold=7)
+        plt.show()
+
+
+# Examples
+# Prokka
+draw_operon_one_contig_assembly("GCA_900635875.1.gbf",
+                                'Prokka', 471058, 476657)
+# #PGAP
+draw_operon_one_contig_assembly("GCA_900635875.1_35661_G01_genomic.gbff",
+                                'PGAP', 471058, 476657)
+
+
+def draw_operon_several_contigs_assembly(gb_file, annotation, record_id, start,
+                                         stop):
+    """
+    Function produces schematic plot of DNA region specified with coordinates
+    by any (Prokka or PGAP) annotation of assembly in two ar more contigs;
+    :param gb_file: path to GB annotation file
+    :param annotation: name annotation: Prokka or PGAP
+    :param record_id: LOCUS from GB file
+    :param start: start coordinate of the desired region
+    :param stop: end coordinate of the desired region
+    :return: None
+    """
+
+    for record in SeqIO.parse(gb_file, 'genbank'):
+        if record.id == record_id:
+            if annotation == "PGAP":
+                graphic_translator = BiopythonTranslator()
+                graphic_translator.ignored_features_types = ['gene']
+                graphic_record = graphic_translator.translate_record(record)
+                graphic_record.crop((start, stop)).plot(figure_width=10,
+                                                        strand_in_label_threshold=7)
+                plt.show()
+
+            if annotation == "Prokka":
+                graphic_translator = BiopythonTranslator()
+                graphic_record = graphic_translator.translate_record(record)
+                graphic_record.crop((start, stop)).plot(figure_width=10,
+                                                        strand_in_label_threshold=7)
+                plt.show()
+
+# Examples
+# Prokka
+draw_operon_several_contigs_assembly("GCA_001874625.1.gbf", "Prokka",
+                                     "CP017671.1", 4509039, 4521933)
+# PGAP
+draw_operon_several_contigs_assembly("GCF_010669105.1_ASM1066910v1_genomic.gbff",
+                                     "PGAP", "NZ_CP048621.1", 4272652,  4285534)
