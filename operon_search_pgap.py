63dc3bca0332565cabeda48470f0a6562917c04b
rybinaanya <43316993+rybinaanya@users.noreply.github.com>
2021-06-11 00:31:05 +0300

    added drawing func

--- operon_search_pgap.py
+++ operon_search_pgap.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from dna_features_viewer import GraphicFeature, GraphicRecord, BiopythonTranslator
 
 
 O_ag_gene_list = ['yibK', 'wzc', 'wzb', 'wza', 'galE', 'gne', 'wpaD',
@@ -56,12 +57,33 @@ def get_operon_boundaries(operons_df, operon_numbers_list):
     return tuple(map(int, [operon_boundaries[0][0], operon_boundaries[1][1]]))
 
 
-# Example
-coord_genes_array = find_o_ag_genes_data_from_gff('GCF_000973125.1_ASM97312v1_genomic.gff', O_ag_gene_list)
+def draw_region_by_coordinates(gff_file, start_, end_):
+    """
+    Function produces schematic plot of DNA region specified with coordinates;
+    visualizes only gene feature types
+    and labels them by gene or its product.
+    :param gff_file: path to GFF annotation file from PGAP
+    :param start_: start coordinate of the desired region
+    :param end_: end coordinate of the desired region
+    :return: None
+    """
+    biopython_translator = BiopythonTranslator()
+    biopython_translator.ignored_features_types = ['CDS']
+    biopython_translator.label_fields = ["gene", "product"]
+    graphic_record = biopython_translator.translate_record(gff_file)
 
-# Parse output of operon-mapper
-operons_df = pd.read_csv("59353/list_of_operons_59353", sep='\t')
-operons_df["Operon"] = operons_df["Operon"].ffill()
+    graphic_record.crop((start_, end_)).plot()  
 
-selected_operons = find_o_ag_operon_numbers(operons_df, coord_genes_array)
-start, end = get_operon_boundaries(operons_df, selected_operons[:2])
+if __name__ == '__main__':
+  # Example
+  coord_genes_array = find_o_ag_genes_data_from_gff('GCF_000973125.1_ASM97312v1_genomic.gff', O_ag_gene_list)
+
+  # Parse output of operon-mapper
+  operons_df = pd.read_csv("59353/list_of_operons_59353", sep='\t')
+  operons_df["Operon"] = operons_df["Operon"].ffill()
+
+  selected_operons = find_o_ag_operon_numbers(operons_df, coord_genes_array)
+  start, end = get_operon_boundaries(operons_df, selected_operons[:2])
+  
+  # Visualize the first pair of operons from list 'selected_operons'
+  draw_region_by_coordinates(gff_file, start, end)
