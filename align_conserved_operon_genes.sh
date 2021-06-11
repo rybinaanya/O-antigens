5615e011cb10e6b71d2060a9d1eb1e8fbb0d1b1c
Anna <70780981+AnyaChurkina@users.noreply.github.com>
2021-06-11 17:45:39 +0300

    Update align_conserved_operon_genes.sh

--- align_conserved_operon_genes.sh
+++ align_conserved_operon_genes.sh
@@ -9,7 +9,7 @@ for f in $(pwd)/Providencia_selected/*; do
 	gunzip ${f}/*.gff.gz; done
 
 echo -e "***Starting to run python script get_conserved_operon_fasta.py to extract conserved wec operon genes\n"
-for species in $(pwd)/Providencia_selected/GCF*; do echo "Looking for wec operon genes in ${species} assembly..."; python3 get_conserved_operon_fasta_2.py "${species}"/*.gff $(pwd)/Providencia_selected/conserved_genes wec; echo -e "Done with ${species} assembly\n"; done
+for species in $(pwd)/Providencia_selected/GCF*; do echo "Looking for wec operon genes in ${species} assembly..."; python3 get_conserved_operon_fasta.py "${species}"/*.gff $(pwd)/Providencia_selected/conserved_genes wec; echo -e "Done with ${species} assembly\n"; done
 echo -e "***All multi-fasta files with wec operon genes have been written and located in $(pwd)/Providencia_selected/conserved_genes\n"
 
 echo -e "***Starting to run python script get_conserved_operon_fasta.py to extract conserved gal operon genes\n"
@@ -19,7 +19,7 @@ do
     if grep -Fxq "galR" ${f}/*gff
     then
         echo "Looking for gal operon genes in ${species} assembly..."
-        python3 get_conserved_operon_fasta_2.py $(pwd)/Providencia_selected/${species}/*.gff $(pwd)/Providencia_selected/conserved_genes gal
+        python3 get_conserved_operon_fasta.py $(pwd)/Providencia_selected/${species}/*.gff $(pwd)/Providencia_selected/conserved_genes gal
         echo -e "Done with ${species} assembly\n"
     else
         echo "gal operon not found in ${f} assembly"
